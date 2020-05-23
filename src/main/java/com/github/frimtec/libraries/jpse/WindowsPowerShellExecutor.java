df64b47c5659f6a3d6a2645e617ee1bc50dd5b01
Markus Friedli <frimtec@gmx.ch>
2020-05-23 13:46:00 +0200

    Add tests

--- src/main/java/com/github/frimtec/libraries/jpse/WindowsPowerShellExecutor.java
+++ src/main/java/com/github/frimtec/libraries/jpse/WindowsPowerShellExecutor.java
@@ -11,7 +11,7 @@ import static java.nio.charset.StandardCharsets.UTF_16LE;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
-final class WindowsPowerShellExecutor implements PowerShellExecutor {
+class WindowsPowerShellExecutor implements PowerShellExecutor {
 
     private static final String POWER_SHELL_CMD = "powershell.exe";
     private static final Base64.Encoder BASE64_ENCODER = Base64.getEncoder();
@@ -32,32 +32,23 @@ final class WindowsPowerShellExecutor implements PowerShellExecutor {
         this.executionCommand = executionCommand;
     }
 
+    @Override
     public ExecutionResult execute(String command) {
         String encodedCommand = BASE64_ENCODER.encodeToString(command.getBytes(UTF_16LE));
-        return execute(new ProcessBuilder(executionCommand, "-EncodedCommand", encodedCommand));
+        return execute(createProcessBuilder(Arrays.asList(executionCommand, "-EncodedCommand", encodedCommand)));
     }
 
+    @Override
     public ExecutionResult execute(Path script, Map<String, String> arguments) {
         List<String> commandLine = new ArrayList<>(Arrays.asList(executionCommand, "-File", script.toString()));
         commandLine.addAll(arguments.entrySet()
                 .stream()
                 .flatMap(entry -> Stream.of("-" + entry.getKey(), "\"" + entry.getValue() + "\""))
                 .collect(Collectors.toList()));
-        return execute(new ProcessBuilder(commandLine));
+        return execute(createProcessBuilder(commandLine));
     }
 
-    private ExecutionResult execute(ProcessBuilder processBuilder) {
-        try {
-            Process process = processBuilder.start();
-            return new ExecutionResultImpl(process.waitFor(), readStream(process.getInputStream()), readStream(process.getErrorStream()));
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException("Wait for process termination interrupted", e);
-        } catch (IOException e) {
-            throw new UncheckedIOException("Powershell cannot be executed", e);
-        }
-    }
-
+    @Override
     public ExecutionResult execute(InputStream script, Map<String, String> arguments) {
         try {
             Path tempFile = Files.createTempFile("java-power-shell-", ".ps1");
@@ -69,6 +60,22 @@ final class WindowsPowerShellExecutor implements PowerShellExecutor {
             }
         } catch (IOException e) {
             throw new UncheckedIOException("Cannot handle script", e);
+        }
+    }
+
+    protected ProcessBuilder createProcessBuilder(List<String> commandLine) {
+        return new ProcessBuilder(commandLine);
+    }
+
+    private ExecutionResult execute(ProcessBuilder processBuilder) {
+        try {
+            Process process = processBuilder.start();
+            return new ExecutionResultImpl(process.waitFor(), readStream(process.getInputStream()), readStream(process.getErrorStream()));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Wait for process termination interrupted", e);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Powershell cannot be executed", e);
         }
     }
 
--- src/test/java/com/github/frimtec/libraries/jpse/WindowsPowerShellExecutorTest.java
+++ src/test/java/com/github/frimtec/libraries/jpse/WindowsPowerShellExecutorTest.java
@@ -1,13 +1,19 @@
 package com.github.frimtec.libraries.jpse;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class WindowsPowerShellExecutorTest {
 
@@ -15,13 +21,72 @@ class WindowsPowerShellExecutorTest {
     private final PowerShellExecutor executor = osWindows ? new WindowsPowerShellExecutor() : new WindowsPowerShellExecutor("echo");
 
     @Test
-    void execute() throws IOException {
+    void instance()  {
+        // act
+        WindowsPowerShellExecutor instance = WindowsPowerShellExecutor.instance();
+
+        // assert
+        assertThat(instance).isNotNull();
+    }
+
+    @Test
+    void executeForSuccess() throws IOException {
         // act
         ExecutionResult executionResult = executor.execute("Write-Host Hello PowerShell!");
 
         // assert
         assertThat(executionResult.isSuccess()).isTrue();
         assertThat(executionResult.getStandartOutput()).isEqualTo(osWindows ? "Hello PowerShell!" : "-EncodedCommand VwByAGkAdABlAC0ASABvAHMAdAAgAEgAZQBsAGwAbwAgAFAAbwB3AGUAcgBTAGgAZQBsAGwAIQA=");
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void executeForFailure() throws IOException {
+        // act
+        ExecutionResult executionResult = executor.execute("Write-Error Problem");
+
+        // assert
+        assertThat(executionResult.isSuccess()).isFalse();
+        assertThat(executionResult.getReturnCode()).isEqualTo(1);
+        assertThat(executionResult.getStandartOutput()).isEmpty();
+        assertThat(executionResult.getErrorOutput()).contains("Problem");
+    }
+
+    @Test
+    void executeForInterruptThrowsException() throws IOException {
+        // arrange
+        Thread mainThread = Thread.currentThread();
+        new Thread(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                fail("Test was interrupted");
+            }
+            mainThread.interrupt();
+        }).start();
+
+        //noinspection MethodDoesntCallSuperMethod
+        PowerShellExecutor interruptedExecutor = osWindows ? executor : new WindowsPowerShellExecutor("") {
+            @Override
+            protected ProcessBuilder createProcessBuilder(List<String> commandLine) {
+                return new ProcessBuilder("sleep", "2");
+            }
+        };
+
+        // act & assert
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> interruptedExecutor.execute("Start-Sleep -Seconds 2"));
+        assertThat(exception.getMessage()).contains("Wait for process termination interrupted");
+    }
+
+    @Test
+    void executeForBadPowerShellExeThrowsException() throws IOException {
+        // arrange
+        PowerShellExecutor errorExecutor = new WindowsPowerShellExecutor("unknown_command.exe");
+
+        // act & assert
+        UncheckedIOException exception = assertThrows(UncheckedIOException.class, () -> errorExecutor.execute("Write-Host any"));
+        assertThat(exception.getMessage()).contains("Powershell cannot be executed");
     }
 
     @Test
