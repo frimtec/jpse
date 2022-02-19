d5f5eb15c08499329968822108347bb3807088a6
Markus Friedli <frimtec@gmx.ch>
2022-02-19 15:29:27 +0100

    Remove obsolete imports

--- src/test/java/com/github/frimtec/libraries/jpse/LinuxPowerShellExecutorTest.java
+++ src/test/java/com/github/frimtec/libraries/jpse/LinuxPowerShellExecutorTest.java
@@ -3,7 +3,6 @@ package com.github.frimtec.libraries.jpse;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class LinuxPowerShellExecutorTest {
     @Test
